2e96a99c24a2ab5bdb34a7e0fec9a82bfd7a42d3
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2025-01-16 20:17:15 +0200

    add pagination js

--- src/js/baseMarkUp.js
+++ src/js/baseMarkUp.js
@@ -1,22 +1,35 @@
 import EventsApiService from './fetchEvents';
 import markUpEvents from '../templates/card';
+import { renderPagination } from './pagination';
 
 const list = document.getElementById('js-list');
+const paginationList = document.querySelector('.pagination__list');
 
 const eventsApiService = new EventsApiService();
 
 renderEvent();
 
 export default async function renderEvent() {
-    try {
-        const data = await eventsApiService.fetchEvents();
-        const events = data._embedded?.events || [];
-        const markUp = markUpEvents(events);
+  try {
+    const data = await eventsApiService.fetchEvents();
+    const events = data._embedded?.events || [];
+    const markUp = markUpEvents(events);
 
-        list.insertAdjacentHTML('beforeend', markUp);
-
-    } catch (error) {
-        console.error(error);
-    }
+    list.insertAdjacentHTML('beforeend', markUp);
+    const totalPages = data.page.totalPages;
+    renderPagination(totalPages, eventsApiService.page, onPageClick);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+function clearEventsList() {
+  list.innerHTML = '';
+}
+
+function onPageClick(newPage) {
+  if (newPage === eventsApiService.page) return;
+  eventsApiService.page = newPage;
+  clearEventsList();
+  renderEvent();
+}
