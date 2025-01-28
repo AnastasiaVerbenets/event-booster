edf7b07218aeea64d31238d790bfd0b1b4fa4dbf
AnastasiaVerbenets <anastasiaverbenets@gmail.com>
2025-01-28 17:22:30 +0000

    Deploying to gh-pages from @ AnastasiaVerbenets/event-booster@ef6ab4e9800c5aa782b9b4bd207d7a7223037af0 🚀

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var f=e=>{throw TypeError(e)};var k=(e,s,t)=>s.has(e)||f("Cannot "+t);var l=(e,s,t)=>(k(e,s,"read from private field"),t?t.call(e):s.get(e)),m=(e,s,t)=>s.has(e)?f("Cannot add the same private member more than once"):s instanceof WeakSet?s.add(e):s.set(e,t);(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))o(a);new MutationObserver(a=>{for(const n of a)if(n.type==="childList")for(const r of n.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&o(r)}).observe(document,{childList:!0,subtree:!0});function t(a){const n={};return a.integrity&&(n.integrity=a.integrity),a.referrerPolicy&&(n.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?n.credentials="include":a.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function o(a){if(a.ep)return;a.ep=!0;const n=t(a);fetch(a.href,n)}})();var d,i;class g{constructor(){m(this,d,"cAC0PQfupJc4LZexavuatT33ADOVunkK");m(this,i,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=0,this.pageSize=12,this.searchQuery=""}async fetchEvents({id:s=null}={}){const t=new URLSearchParams({apikey:l(this,d),page:this.page,size:this.pageSize,keyword:this.searchQuery}),o=s?`${l(this,i)}/${s}?${t}`:`${l(this,i)}?${t}&classificationName=music`,a=await fetch(o);if(!a.ok)throw new Error("Failed to fetch events");return a.json()}}d=new WeakMap,i=new WeakMap;function L(e){return e.map(t=>{var _,p;const o=((_=t.images[0])==null?void 0:_.url)||"",a=t.name||"No name avaible",n=t.dates.start.localDate||"Unknown Date",r=((p=t._embedded)==null?void 0:p.venues[0].name)||"Unknown Location",E=t.url||"#";return`
-<li class="cards__item" data-id="${t.id}">
-          <img src="${o}" alt="${a}" class="cards__img" />
+var v=e=>{throw TypeError(e)};var k=(e,t,s)=>t.has(e)||v("Cannot "+s);var m=(e,t,s)=>(k(e,t,"read from private field"),s?s.call(e):t.get(e)),p=(e,t,s)=>t.has(e)?v("Cannot add the same private member more than once"):t instanceof WeakSet?t.add(e):t.set(e,s);(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))n(a);new MutationObserver(a=>{for(const o of a)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(a){const o={};return a.integrity&&(o.integrity=a.integrity),a.referrerPolicy&&(o.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?o.credentials="include":a.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function n(a){if(a.ep)return;a.ep=!0;const o=s(a);fetch(a.href,o)}})();var u,_;class y{constructor(){p(this,u,"cAC0PQfupJc4LZexavuatT33ADOVunkK");p(this,_,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=0,this.pageSize=12,this.searchQuery=""}async fetchEvents(){const t=new URLSearchParams({apikey:m(this,u),page:this.page,size:this.pageSize,keyword:this.searchQuery}),s=`${m(this,_)}?${t}&classificationName=music`,n=await fetch(s);if(!n.ok)throw new Error("Failed to fetch events");return n.json()}async fetchEventsById(t){const s=new URLSearchParams({apikey:m(this,u),page:this.page,size:this.pageSize,keyword:this.searchQuery}),n=`${m(this,_)}/${t}?${s}`,a=await fetch(n);if(!a.ok)throw new Error("Failed to fetch events");return a.json()}}u=new WeakMap,_=new WeakMap;function w(e){return e.map(s=>{var l,d;const n=((l=s.images[0])==null?void 0:l.url)||"",a=s.name||"No name avaible",o=s.dates.start.localDate||"Unknown Date",c=((d=s._embedded)==null?void 0:d.venues[0].name)||"Unknown Location",i=s.url||"#";return`
+<li class="cards__item" data-id="${s.id}">
+          <img src="${n}" alt="${a}" class="cards__img" />
           <h4 class="cards__event">${a}</h4>
-          <p class="cards__date">${n}</p>
-        <a href="${E}" target="_blank" rel="noopener noreferrer" class="cards__location">
+          <p class="cards__date">${o}</p>
+        <a href="${i}" target="_blank" rel="noopener noreferrer" class="cards__location">
           <svg class="cards__icon">
             <use href="/imgs/icons/symbol-defs.svg#location"></use>
           </svg>
-          ${r}
+          ${c}
         </a>
       </li>
-    `}).join("")}const h=document.getElementById("js-list"),U=document.querySelector(".pagination__list"),v=document.getElementById("searchInput");document.getElementById("headerForm");const u=new g;v.addEventListener("input",x);async function x(e){e.preventDefault(),u.searchQuery=v.value.trim(),u.page=0,A(),S(),await y()}y();async function y(){var e;try{const t=((e=(await u.fetchEvents())._embedded)==null?void 0:e.events)||[],o=L(t);h.insertAdjacentHTML("beforeend",o)}catch(s){console.error(s)}}function A(){h.innerHTML=""}function S(){U.innerHTML=""}function w(e){return`
+    `}).join("")}const b=document.getElementById("js-list"),U=document.querySelector(".pagination__list"),E=document.getElementById("searchInput");document.getElementById("headerForm");const h=new y;E.addEventListener("input",I);async function I(e){e.preventDefault(),h.searchQuery=E.value.trim(),h.page=0,S(),P(),await L()}L();async function L(){var e;try{const s=((e=(await h.fetchEvents())._embedded)==null?void 0:e.events)||[],n=w(s);b.insertAdjacentHTML("beforeend",n)}catch(t){console.error(t)}}function S(){b.innerHTML=""}function P(){U.innerHTML=""}function O(e){var l,d,f,g;const t=((l=e.images[0])==null?void 0:l.url)||"",s=e.description||"No description available",n=e.dates.start.localDate||"Unknown Date",a=((d=e._embedded.venues[0])==null?void 0:d.name)||"Unknown Location",o=((f=e._embedded.venues[0])==null?void 0:f.city.name)||"",c=((g=e._embedded.venues[0])==null?void 0:g.country.name)||"",i=e.name||"No name avaible";return`
     <div class="modal" id="js-modal">
         <button type="button" class="modal__btn-close">
             <svg class="modal__icon-close">
@@ -18,46 +18,36 @@ var f=e=>{throw TypeError(e)};var k=(e,s,t)=>s.has(e)||f("Cannot "+t);var l=(e,s
             </svg>
         </button>
 
-        <img src="/imgs/modal/artist.png" alt="" class="modal__img" />
+        <img src="${t}" alt="${i}" class="modal__img" />
 
         <div class="modal__box">
-            <img src="/imgs/modal/main-img.png" alt="" class="modal__main-img" />
+            <img src="${t}" alt="${i}" class="modal__main-img" />
 
             <div class="modal__info">
                 <h4 class="modal__title">INFO</h4>
-                <p class="modal__text">Atlas Weekend is the largest music festival in Ukraine.
-                    More than 200 artists will create a proper music festival atmosphere on 10 stages</p>
+                <p class="modal__text">${s}</p>
 
                 <h4 class="modal__title">WHEN</h4>
-                <p class="modal__text modal__text--none">2021-06-09</p>
-                <p class="modal__text">20:00 (Kyiv/Ukraine)</p>
+                <p class="modal__text">${n}</p>
+                <p class="modal__text">${time}</p>
 
                 <h4 class="modal__title">WHERE</h4>
-                <p class="modal__text modal__text--none">Kyiv, Ukraine</p>
-                <p class="modal__text">VDNH</p>
+                <p class="modal__text">${a}</p>
+                <p class="modal__text">${o}, ${c}</p>
 
                 <h4 class="modal__title">WHO</h4>
-                <p class="modal__text">The Black Eyed Peas</p>
+                <p class="modal__text">${i}</p>
 
                 <h4 class="modal__title">PRICES</h4>
-                <p class="modal__tex modal__text-ticket">
-                    <svg class="modal__ticket">
-                        <use href="../imgs/icons/symbol-defs.svg#ticket"></use>
-                    </svg> Standart 300-500 UAH
-                </p>
-                <button class="modal__buy-btn">BUY TICKETS</button>
-
                 <p class="modal__text modal__text-ticket">
                     <svg class="modal__ticket">
-                        <use href="../imgs/icons/symbol-defs.svg#ticket"></use>
-                    </svg>
-                    VIP 1000-1500 UAH
+                        <use href="/imgs/icons/symbol-defs.svg#ticket"></use>
+                    </svg> Prices vary
                 </p>
                 <button class="modal__buy-btn">BUY TICKETS</button>
             </div>
         </div>
         <button class="modal__more-btn">MORE FROM THIS AUTHOR</button>
-
     </div>
-    `}const I=document.getElementById("js-list"),c=document.getElementById("modal-card"),H=new g;I.addEventListener("click",T);async function T(e){const s=e.target.closest(".cards__item");if(s){s.dataset.id;try{const t=await H.fetchEvents("1a9Zk7Y34xAZe56"),o=w(t);c.insertAdjacentHTML("beforeend",o),c.classList.remove("hidden"),c.addEventListener("click",b)}catch(t){console.error(t)}}}function b(e){(e.target.classList.contains("modal__btn-close")||e.target===c)&&(c.classList.add("hidden"),c.removeEventListener("click",b),c.innerHTML="")}
+    `}const A=document.getElementById("js-list"),r=document.getElementById("modal-card"),M=new y;A.addEventListener("click",x);async function x(e){const t=e.target.closest(".cards__item");if(!t)return;const s=t.dataset.id;console.log(s);try{const n=await M.fetchEventsById(s),a=O(n);r.insertAdjacentHTML("beforeend",a),r.classList.remove("hidden"),r.addEventListener("click",$)}catch(n){console.error(n)}}function $(e){(e.target.classList.contains("modal__btn-close")||e.target===r)&&(r.classList.add("hidden"),r.removeEventListener("click",$),r.innerHTML="")}
 //# sourceMappingURL=index.js.map
