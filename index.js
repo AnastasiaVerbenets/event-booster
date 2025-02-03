92a605b88df594cbeec51ed75f2c06e43e059623
AnastasiaVerbenets <anastasiaverbenets@gmail.com>
2025-02-03 15:41:02 +0000

    Deploying to gh-pages from @ AnastasiaVerbenets/event-booster@82633695f65edaf9bfed0c5139dee9bd9bf9d171 🚀

--- index.js
+++ index.js
@@ -1,101 +1,75 @@
-var b=t=>{throw TypeError(t)};var j=(t,e,s)=>e.has(t)||b("Cannot "+s);var E=(t,e,s)=>(j(t,e,"read from private field"),s?s.call(t):e.get(t)),f=(t,e,s)=>e.has(t)?b("Cannot add the same private member more than once"):e instanceof WeakSet?e.add(t):e.set(t,s);(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))n(a);new MutationObserver(a=>{for(const i of a)if(i.type==="childList")for(const o of i.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function s(a){const i={};return a.integrity&&(i.integrity=a.integrity),a.referrerPolicy&&(i.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?i.credentials="include":a.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(a){if(a.ep)return;a.ep=!0;const i=s(a);fetch(a.href,i)}})();var h,_;class H{constructor(){f(this,h,"cAC0PQfupJc4LZexavuatT33ADOVunkK");f(this,_,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=0,this.pageSize=12,this.searchQuery=""}async fetchEvents(){const e=new URLSearchParams({apikey:E(this,h),page:this.page,size:this.pageSize,keyword:this.searchQuery,sort:"random"}),s=`${E(this,_)}?${e}&classificationName=music`,n=await fetch(s);if(!n.ok)throw new Error("Failed to fetch events");return n.json()}async fetchEventsById(e){const s=new URLSearchParams({apikey:E(this,h)}),n=`https://app.ticketmaster.com/discovery/v2/events/${e}.json?${s}`,a=await fetch(n);if(!a.ok)throw new Error("Failed to fetch events");return a.json()}}h=new WeakMap,_=new WeakMap;function P(t){return t.map(s=>{var c,m;const n=((c=s.images[2])==null?void 0:c.url)||"",a=s.name||"No name avaible",i=s.dates.start.localDate||"Unknown Date",o=((m=s._embedded)==null?void 0:m.venues[0].name)||"Unknown Location",l=s.url||"#";return`
-      <li class="cards__item" data-id="${s.id}">
-          <img src="${n}" alt="${a}" class="cards__img" />
-          <h4 class="cards__event">${a}</h4>
-          <p class="cards__date">${i}</p>
-        <a href="${l}" target="_blank" rel="noopener noreferrer" class="cards__location">
-          <svg class="cards__icon">
-            <use href="/imgs/icons/symbol-defs.svg#location"></use>
-          </svg>${o}
+var H=t=>{throw TypeError(t)};var q=(t,e,n)=>e.has(t)||H("Cannot "+n);var E=(t,e,n)=>(q(t,e,"read from private field"),n?n.call(t):e.get(t)),w=(t,e,n)=>e.has(t)?H("Cannot add the same private member more than once"):e instanceof WeakSet?e.add(t):e.set(t,n);(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const o of s)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function n(s){const o={};return s.integrity&&(o.integrity=s.integrity),s.referrerPolicy&&(o.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?o.credentials="include":s.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(s){if(s.ep)return;s.ep=!0;const o=n(s);fetch(s.href,o)}})();var f,y;class I{constructor(){w(this,f,"cAC0PQfupJc4LZexavuatT33ADOVunkK");w(this,y,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=0,this.pageSize=12,this.searchQuery=""}async fetchEvents(){const e=new URLSearchParams({apikey:E(this,f),page:this.page,size:this.pageSize,keyword:this.searchQuery,sort:"random"}),n=`${E(this,y)}?${e}&classificationName=music`,a=await fetch(n);if(!a.ok)throw new Error("Failed to fetch events");return a.json()}async fetchEventsById(e){const n=new URLSearchParams({apikey:E(this,f)}),a=`https://app.ticketmaster.com/discovery/v2/events/${e}.json?${n}`,s=await fetch(a);if(!s.ok)throw new Error("Failed to fetch events");return s.json()}}f=new WeakMap,y=new WeakMap;function z(t){return t.map(n=>{var l,d;const a=((l=n.images[2])==null?void 0:l.url)||"",s=n.name||"No name avaible",o=n.dates.start.localDate||"Unknown Date",i=((d=n._embedded)==null?void 0:d.venues[0].name)||"Unknown Location",c=n.url||"#";return`
+      <li class="cards__item" data-id="${n.id}">
+          <img src="${a}" alt="${s}" class="cards__img" />
+          <h4 class="cards__event">${s}</h4>
+          <p class="cards__date">${o}</p>
+          <a href="${c}" target="_blank" rel="noopener noreferrer" class="cards__location">
+          <svg class="cards__icon" xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
+              <path d="M3.5 0C1.57011 0 0 1.55933 0 3.47595C0 5.88495 3.50344 10 3.50344 10C3.50344 10 7 5.76648 7 3.47595C7 1.55933 5.42995 0 3.5 0ZM4.55602 4.49371C4.26484 4.78284 3.88245 4.92743 3.5 4.92743C3.11761 4.92743 2.7351 4.78284 2.44404 4.49371C1.86173 3.91547 1.86173 2.97455 2.44404 2.39624C2.72601 2.11609 3.10108 1.96179 3.5 1.96179C3.89892 1.96179 4.27393 2.11615 4.55602 2.39624C5.13833 2.97455 5.13833 3.91547 4.55602 4.49371Z" fill="white"/>
+          </svg>
+          ${i}
         </a>
       </li>
-    `}).join("")}function R(t,e,s){const n=document.querySelector(".pagination__list");n.innerHTML="";const i=F(t,e).map(l=>l==="..."?`<li class="pagination__list__item">
+    `}).join("")}function W(t,e,n){const a=document.querySelector(".pagination__list");a.innerHTML="";const o=Y(t,e).map(c=>c==="..."?`<li class="pagination__list__item">
           <span class="pagination__dots">...</span>
         </li>`:`<li class="pagination__list__item">
-        <button class="pagination__list__button ${l===e?"active":""}" data-page="${l}">${l+1}</button>
-      </li>`).join("");n.insertAdjacentHTML("beforeend",i),n.addEventListener("click",o);function o(l){if(l.target.tagName!=="BUTTON")return;const c=Number(l.target.dataset.page);c!==e&&s(c)}}function F(t,e){const s=[];if(t<=5)for(let a=0;a<t;a++)s.push(a);else{e>2&&s.push(0),e>3&&s.push("...");const a=Math.max(0,e-1),i=Math.min(t,e+2);for(let o=a;o<i;o++)s.push(o);e<t-3&&s.push("..."),e<t-2&&s.push(t-1)}return s}const x=document.getElementById("js-list"),D=document.querySelector(".pagination__list"),$=document.getElementById("searchInput");document.getElementById("headerForm");const p=new H;g();$.addEventListener("input",K);async function K(t){t.preventDefault(),p.searchQuery=$.value.trim(),p.page=0,k(),V(),await g()}async function g(){var t;try{const e=await p.fetchEvents(),s=((t=e._embedded)==null?void 0:t.events)||[],n=P(s);x.insertAdjacentHTML("beforeend",n);const a=e.page.totalPages;R(a,p.page,Q)}catch(e){console.error(e.status)}}function k(){x.innerHTML=""}function Q(t){t!==p.page&&(p.page=t,k(),g())}function V(){D.innerHTML=""}function q(t){var v,M,w,y;t.url;const e=((v=t.images[2])==null?void 0:v.url)||"",s=t.info||t.description||"No description available",n=t.dates.start.localDate||"Unknown Date",a=O(t.dates.start.localTime),i=((M=t._embedded.venues[0])==null?void 0:M.name)||"Unknown Location",o=((w=t._embedded.venues[0])==null?void 0:w.city.name)||"",l=((y=t._embedded.venues[0])==null?void 0:y.country.name)||"",c=t.name||"No name avaible",m=t.priceRanges||[],u=m.find(d=>d.type==="standard")||"",L=m.find(d=>d.type==="vip")||"",B=Math.round(u.min),U=Math.round(u.max),C=Math.round(L.min),T=Math.round(L.max),I=u?`
+        <button class="pagination__list__button ${c===e?"active":""}" data-page="${c}">${c+1}</button>
+      </li>`).join("");a.insertAdjacentHTML("beforeend",o),a.addEventListener("click",i);function i(c){if(c.target.tagName!=="BUTTON")return;const l=Number(c.target.dataset.page);l!==e&&n(l)}}function Y(t,e){const n=[];if(t<=5)for(let s=0;s<t;s++)n.push(s);else{e>2&&n.push(0),e>3&&n.push("...");const s=Math.max(0,e-1),o=Math.min(t,e+2);for(let i=s;i<o;i++)n.push(i);e<t-3&&n.push("..."),e<t-2&&n.push(t-1)}return n}const T=document.getElementById("js-list"),J=document.querySelector(".pagination__list"),S=document.getElementById("searchInput");document.getElementById("headerForm");const h=new I;M();S.addEventListener("input",G);async function G(t){t.preventDefault(),h.searchQuery=S.value.trim(),h.page=0,A(),tt(),await M()}async function M(){var t;try{const e=await h.fetchEvents(),n=((t=e._embedded)==null?void 0:t.events)||[],a=z(n);T.insertAdjacentHTML("beforeend",a);const s=e.page.totalPages;W(s,h.page,X)}catch(e){console.error(e.status)}}function A(){T.innerHTML=""}function X(t){t!==h.page&&(h.page=t,A(),M())}function tt(){J.innerHTML=""}function et(t){var C,x,U,B;const e=((C=t.images[2])==null?void 0:C.url)||"",n=t.info||t.description||"No description available",a=Q(n),s=t.dates.start.localDate||"Unknown Date",o=K(t.dates.start.localTime),i=((x=t._embedded.venues[0])==null?void 0:x.name)||"Unknown Location",c=((U=t._embedded.venues[0])==null?void 0:U.city.name)||"",l=((B=t._embedded.venues[0])==null?void 0:B.country.name)||"",d=t.name||"No name available",k=t.priceRanges||[],g=k.find(r=>r.type==="standard")||"",v=k.find(r=>r.type==="vip")||"",j=Math.round(g.min),N=Math.round(g.max),Z=Math.round(v.min),R=Math.round(v.max),P=`https://www.ticketmaster.com/search?q=${encodeURIComponent(d)}`,L=`
+    <svg class="modal__ticket" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 17" fill="none">
+    <path d="M2.66668 0.833496L0 0.833496L0 16.8335H2.66668L2.66668 0.833496Z" fill="#0E0E0E" />
+    <path d="M9.3737 0.833496L6.70703 0.833496L6.70703 16.8335H9.3737L9.3737 0.833496Z" fill="#0E0E0E" />
+    <path d="M13.4142 0.833496L10.7476 0.833496L10.7476 16.8335H13.4142L13.4142 0.833496Z" fill="#0E0E0E" />
+    <path d="M24.0001 0.833496L20.0405 0.833496L20.0405 16.8335H24.0001L24.0001 0.833496Z" fill="#0E0E0E" />
+    <path d="M5.33334 0.833496L4.04041 0.833496L4.04041 16.8335H5.33334L5.33334 0.833496Z" fill="#0E0E0E" />
+    <path d="M16 0.833496L14.707 0.833496L14.707 16.8335H16L16 0.833496Z" fill="#0E0E0E" />
+    <path d="M18.6666 0.833496L17.3737 0.833496L17.3737 16.8335H18.6666L18.6666 0.833496Z" fill="#0E0E0E" />
+    </svg>`,D=g?`
         <p class="modal__text modal__text-ticket ">
-    <svg class="modal__ticket" xmlns="http://www.w3.org/2000/svg" width="24" height="17" viewBox="0 0 24 17" fill="none">
-        <path d="M2.66668 0.833496L0 0.833496L0 16.8335H2.66668L2.66668 0.833496Z" fill="#0E0E0E" />
-        <path d="M9.3737 0.833496L6.70703 0.833496L6.70703 16.8335H9.3737L9.3737 0.833496Z" fill="#0E0E0E" />
-        <path d="M13.4142 0.833496L10.7476 0.833496L10.7476 16.8335H13.4142L13.4142 0.833496Z" fill="#0E0E0E" />
-        <path d="M24.0001 0.833496L20.0405 0.833496L20.0405 16.8335H24.0001L24.0001 0.833496Z" fill="#0E0E0E" />
-        <path d="M5.33334 0.833496L4.04041 0.833496L4.04041 16.8335H5.33334L5.33334 0.833496Z" fill="#0E0E0E" />
-        <path d="M16 0.833496L14.707 0.833496L14.707 16.8335H16L16 0.833496Z" fill="#0E0E0E" />
-        <path d="M18.6666 0.833496L17.3737 0.833496L17.3737 16.8335H18.6666L18.6666 0.833496Z" fill="#0E0E0E" />
-    </svg>
-            Standart ${B}-${U} ${u.currency}
+        ${L} Standart ${j}-${N} ${g.currency}
         </p>
-        <button class="modal__buy-btn">BUY TICKETS</button>`:`<p class="modal__text modal__text-ticket modal__text-standart">
-            <svg class="modal__ticket" xmlns="http://www.w3.org/2000/svg" width="24" height="17" viewBox="0 0 24 17" fill="none">
-        <path d="M2.66668 0.833496L0 0.833496L0 16.8335H2.66668L2.66668 0.833496Z" fill="#0E0E0E" />
-        <path d="M9.3737 0.833496L6.70703 0.833496L6.70703 16.8335H9.3737L9.3737 0.833496Z" fill="#0E0E0E" />
-        <path d="M13.4142 0.833496L10.7476 0.833496L10.7476 16.8335H13.4142L13.4142 0.833496Z" fill="#0E0E0E" />
-        <path d="M24.0001 0.833496L20.0405 0.833496L20.0405 16.8335H24.0001L24.0001 0.833496Z" fill="#0E0E0E" />
-        <path d="M5.33334 0.833496L4.04041 0.833496L4.04041 16.8335H5.33334L5.33334 0.833496Z" fill="#0E0E0E" />
-        <path d="M16 0.833496L14.707 0.833496L14.707 16.8335H16L16 0.833496Z" fill="#0E0E0E" />
-        <path d="M18.6666 0.833496L17.3737 0.833496L17.3737 16.8335H18.6666L18.6666 0.833496Z" fill="#0E0E0E" />
-    </svg>
-             Standart tickets are not available
-        </p>`,S=L?`
+        <button class="modal__buy-btn">BUY TICKETS</button>`:`<p class="modal__text modal__text-ticket" style="margin-bottom: 20px;">
+        ${L} Standart tickets are not available
+        </p>`,F=v?`
         <p class="modal__text modal__text-ticket">
-               <svg class="modal__ticket" xmlns="http://www.w3.org/2000/svg" width="24" height="17" viewBox="0 0 24 17" fill="none">
-        <path d="M2.66668 0.833496L0 0.833496L0 16.8335H2.66668L2.66668 0.833496Z" fill="#0E0E0E" />
-        <path d="M9.3737 0.833496L6.70703 0.833496L6.70703 16.8335H9.3737L9.3737 0.833496Z" fill="#0E0E0E" />
-        <path d="M13.4142 0.833496L10.7476 0.833496L10.7476 16.8335H13.4142L13.4142 0.833496Z" fill="#0E0E0E" />
-        <path d="M24.0001 0.833496L20.0405 0.833496L20.0405 16.8335H24.0001L24.0001 0.833496Z" fill="#0E0E0E" />
-        <path d="M5.33334 0.833496L4.04041 0.833496L4.04041 16.8335H5.33334L5.33334 0.833496Z" fill="#0E0E0E" />
-        <path d="M16 0.833496L14.707 0.833496L14.707 16.8335H16L16 0.833496Z" fill="#0E0E0E" />
-        <path d="M18.6666 0.833496L17.3737 0.833496L17.3737 16.8335H18.6666L18.6666 0.833496Z" fill="#0E0E0E" />
-    </svg>
-            VIP ${C}-${T} ${L.currency}
+        ${L} VIP ${Z}-${R} ${v.currency}
         </p>
         <button class="modal__buy-btn">BUY TICKETS</button>`:`<p class="modal__text modal__text-ticket">
-            <svg class="modal__ticket" xmlns="http://www.w3.org/2000/svg" width="24" height="17" viewBox="0 0 24 17" fill="none">
-        <path d="M2.66668 0.833496L0 0.833496L0 16.8335H2.66668L2.66668 0.833496Z" fill="#0E0E0E" />
-        <path d="M9.3737 0.833496L6.70703 0.833496L6.70703 16.8335H9.3737L9.3737 0.833496Z" fill="#0E0E0E" />
-        <path d="M13.4142 0.833496L10.7476 0.833496L10.7476 16.8335H13.4142L13.4142 0.833496Z" fill="#0E0E0E" />
-        <path d="M24.0001 0.833496L20.0405 0.833496L20.0405 16.8335H24.0001L24.0001 0.833496Z" fill="#0E0E0E" />
-        <path d="M5.33334 0.833496L4.04041 0.833496L4.04041 16.8335H5.33334L5.33334 0.833496Z" fill="#0E0E0E" />
-        <path d="M16 0.833496L14.707 0.833496L14.707 16.8335H16L16 0.833496Z" fill="#0E0E0E" />
-        <path d="M18.6666 0.833496L17.3737 0.833496L17.3737 16.8335H18.6666L18.6666 0.833496Z" fill="#0E0E0E" />
-    </svg>
-             VIP tickets are not available
-        </p>`;function O(d){if(!d)return"Unknown Time";const[A,N]=d.split(":");return`${A}:${N}`}return`
+        ${L} VIP tickets are not available
+        </p>`;function K(r){if(!r)return"Unknown Time";const[p,b]=r.split(":");return`${p}:${b}`}function Q(r,p=3,b=30){if(!r)return r;const V=r.split(" "),u=[];let _="";return V.map($=>{u.length<p&&((_+$).length>b?(u.push(_.trim()),_=$+" "):_+=$+" ")}),u.length<p&&_.trim()&&u.push(_.trim()),u.length>=p?u.slice(0,p).join(" ")+"...":r}return`
     <div class="modal">
         <button type="button" class="modal__btn-close" >
-            <svg class="modal__icon-close" xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-                <path d="M0.874187 17C0.65025 17 0.426313 16.9148 0.256172 16.7434C-0.0853907 16.4019 -0.0853907 15.8481 0.256172 15.5066L15.5069 0.256166C15.8485 -0.0853885 16.4023 -0.0853885 16.7438 0.256166C17.0854 0.59772 17.0854 1.15146 16.7438 1.49323L1.49327 16.7434C1.32185 16.9138 1.09791 17 0.874187 17Z" fill="#4C00FE"/>
-                <path d="M16.126 17C15.9021 17 15.6784 16.9148 15.508 16.7434L0.256172 1.49323C-0.0853907 1.15146 -0.0853907 0.59772 0.256172 0.256166C0.597735 -0.0853885 1.15149 -0.0853885 1.49327 0.256166L16.7438 15.5066C17.0854 15.8481 17.0854 16.4019 16.7438 16.7434C16.5724 16.9138 16.3487 17 16.126 17Z" fill="#4C00FE"/>
+            <svg class="modal__icon-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17" >
+                <path d="M0.874187 17C0.65025 17 0.426313 16.9148 0.256172 16.7434C-0.0853907 16.4019 -0.0853907 15.8481 0.256172 15.5066L15.5069 0.256166C15.8485 -0.0853885 16.4023 -0.0853885 16.7438 0.256166C17.0854 0.59772 17.0854 1.15146 16.7438 1.49323L1.49327 16.7434C1.32185 16.9138 1.09791 17 0.874187 17Z" />
+                <path d="M16.126 17C15.9021 17 15.6784 16.9148 15.508 16.7434L0.256172 1.49323C-0.0853907 1.15146 -0.0853907 0.59772 0.256172 0.256166C0.597735 -0.0853885 1.15149 -0.0853885 1.49327 0.256166L16.7438 15.5066C17.0854 15.8481 17.0854 16.4019 16.7438 16.7434C16.5724 16.9138 16.3487 17 16.126 17Z" />
             </svg>
         </button>
 
-        <img src="${e}" alt="${c}" class="modal__img" />
+        <img src="${e}" alt="${d}" class="modal__img" />
 
         <div class="modal__box">
-            <img src="${e}" alt="${c}" class="modal__main-img" />
+            <img src="${e}" alt="${d}" class="modal__main-img" />
 
             <div class="modal__info">
                 <h4 class="modal__title">INFO</h4>
-                <p class="modal__text modal__description ">${s}</p>
+                <p class="modal__text">${a}</p>
 
                 <h4 class="modal__title">WHEN</h4>
-                <p class="modal__text modal__text--none">${n}</p>
-                <p class="modal__text">${a} (${o}/${l})</p>
+                <p class="modal__text modal__text--none">${s}</p>
+                <p class="modal__text">${o} (${c}/${l})</p>
 
                 <h4 class="modal__title">WHERE</h4>
                 <p class="modal__text modal__text--none">${i}</p>
-                <p class="modal__text">${o}, ${l}</p>
+                <p class="modal__text">${c}, ${l}</p>
 
                 <h4 class="modal__title">WHO</h4>
-                <p class="modal__text">${c}</p>
+                <p class="modal__text">${d}</p>
 
                 <h4 class="modal__title">PRICES</h4>
-                ${I}
-                ${S}
+                ${D}
+                ${F}
             </div>
         </div>
-        <button class="modal__more-btn">MORE FROM THIS AUTHOR</button>
-    </div>
-    `}const z=document.getElementById("js-list"),r=document.getElementById("modal-card"),W=new H;z.addEventListener("click",Y);async function Y(t){const e=t.target.closest(".cards__item");if(!e)return;const s=e.dataset.id;console.log(s);try{const n=await W.fetchEventsById(s),a=q(n);r.insertAdjacentHTML("beforeend",a),r.classList.remove("hidden"),r.addEventListener("click",Z)}catch(n){console.error(n)}}function Z(t){(t.target.classList.contains("modal__btn-close")||t.target===r)&&(r.classList.add("hidden"),r.removeEventListener("click",Z),r.innerHTML="")}
+        <button class="modal__more-btn" onclick="window.open('${P}', '_blank')">MORE FROM THIS AUTHOR</button>
+        </div>
+`}const nt=document.getElementById("js-list"),m=document.getElementById("modal-card"),st=new I;nt.addEventListener("click",at);async function at(t){const e=t.target.closest(".cards__item");if(!e)return;const n=e.dataset.id;console.log(n);try{const a=await st.fetchEventsById(n),s=et(a);m.insertAdjacentHTML("beforeend",s),m.classList.remove("hidden"),m.addEventListener("click",O)}catch(a){console.error(a)}}function O(t){const e=t.target.closest(".modal__btn-close"),n=t.target===m;(e||n)&&(m.classList.add("hidden"),m.removeEventListener("click",O),m.innerHTML="")}
 //# sourceMappingURL=index.js.map
